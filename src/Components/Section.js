b4b6879c1af25c8f3baea4e385760eaaa0bea4a4
isurumanod <isurumanod99@gmail.com>
2023-05-22 08:48:22 +0530

    Cerate Components

---
+++ src/Components/Section.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components";
+function Section() {
+    return (
+        <Wrap>
+            <h3>section</h3>
+        </Wrap>
+    );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  height: 100vh;
+
+
+
+`

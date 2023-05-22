8d6bd8e3d255208976ea366655d8366d830dfb9d
isurumanod <isurumanod99@gmail.com>
2023-05-22 13:26:28 +0530

    header ui update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import styled from "styled-components";
+import "./Header.css"
+
 
 function Header() {
+
+    const [darkLink,setDarkLink] = useState(false)
+
+    useEffect(() =>{
+        window.addEventListener("scroll",() =>{
+            if (window.scrollY > 800) {
+                setDarkLink(true)
+            }else setDarkLink(false)
+        })
+
+
+
+
+    },[])
+
+
     return (
         <Container>
             <a href="#">
                 <img className={"header__logo"} src="/images/logo.svg" alt={"logo"}/>
             </a>
             <Menu>
-                <ul className="header__list">
+                <ul className={`header__list ${darkLink && "header__list__dark"}`}>
                     <li className="header__links"><a href="#">Model S</a></li>
-                    <li className="header__links"><a href="#">Model S</a></li>
-                    <li className="header__links"><a href="#">Model S</a></li>
-                    <li className="header__links"><a href="#">Model S</a></li>
+                    <li className="header__links"><a href="#">Model 3</a></li>
+                    <li className="header__links"><a href="#">Model X</a></li>
+                    <li className="header__links"><a href="#">Model Y</a></li>
                 </ul>
             </Menu>
+            <RightMenu>
+                <ul className={`header__list ${darkLink && "header__list__dark"}`}>
+                    <li className="header__links"><a href="#">Shop</a></li>
+                    <li className="header__links"><a href="#">Account </a></li>
+                    <li className="header__links"><a href="#">Menu</a>
+                        </li>
+
+
+                </ul>
+            </RightMenu>
 
 
         </Container>
@@ -31,10 +59,18 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
-align-items: center;
+  align-items: center;
   padding: 0 20px;
 `
 const Menu = styled.div`
-  
-  
+  @media (max-width: 676px) {
+    display: none;
+  }
+
+`
+const RightMenu = styled.div`
+  img {
+    width: 20px;
+  }
+
 `
